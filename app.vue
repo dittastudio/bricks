b08745631ab7f861fbe02aa0aaf853bc55946abc
Michael Pumo <hello@michaelpumo.com>
2024-06-11 16:47:51 +0100

    Absolute footer content.

--- app.vue
+++ app.vue
@@ -23,14 +23,14 @@ useSeoMeta({
 </script>
 
 <template>
-  <div>
+  <div class="relative">
     <NuxtPage />
 
-    <div v-if="story?.content" class="flex flex-col gap-16 w-full px-5 pb-16 sm:px-7">
+    <div v-if="story?.content" class="flex flex-col lg:flex-row gap-16 w-full px-5 pb-16 sm:px-7 lg:absolute lg:right-0 lg:bottom-0 lg:w-1/2 lg:justify-end">
       <address v-if="story?.content?.address && storyblokRichTextContent(story.content.address)">
         <StoryblokRichText
           :content="story.content.address"
-          class="w-full lg:max-w-[50%] [&>*:not(:last-child)]:mb-5 [&>p]:text-18 [&>p]:text-balance [&>p]:sm:text-27"
+          class="w-full [&>*:not(:last-child)]:mb-5 [&>p]:text-18 [&>p]:text-balance [&>p]:sm:text-27"
         />
       </address>
 
